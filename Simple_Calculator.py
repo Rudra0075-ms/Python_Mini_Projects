0e32ecdb0ca77b39c0c1a7aa6a43020a3df95326
Rudra Madhab Sahoo <sahoorudramadhab2007@gmail.com>
2026-08-04 17:44:54 +0530

    Add files via upload

---
+++ Simple_Calculator.py
@@ -0,0 +1,11 @@
+def simple_calc(a,b):
+    print("add(+)=",a+b)
+    print("subtract(-)=",a-b)
+    print("multiply(*)=",a*b)
+    if (b!=0):
+        print("divide(/)=",a/b)
+    else:
+        print("can't divide")    
+a=5
+b=10
+simple_calc(a,b)       
